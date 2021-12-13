f322c5215e614eecb1cbc8286416bf025495a3df
Marcin Szyc <szyc.marcin@gmail.com>
2021-12-13 14:46:50 +0000

    Merge pull request #2 from MarcinSzyc/fastify-adapter
    feat: fastify support added

--- packages/movies3/src/main.ts
+++ packages/movies3/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  try {
+    await app.listen(3000);
+    Logger.log('Server started at port 3000');
+  } catch (error) {
+    Logger.error(error);
+  }
 }
 
 bootstrap();
