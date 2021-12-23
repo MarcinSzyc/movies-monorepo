b089ae79bea5f85770c8d91c238f320a3b628557
Marcin Szyc <szyc.marcin@gmai.com>
2021-12-23 10:38:47 +0100

    feat: tests

--- packages/movies-app/src/authentication/authentication.service.spec.ts
+++ packages/movies-app/src/authentication/authentication.service.spec.ts
@@ -1,12 +1,18 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      providers: [
+        AuthenticationService,
+        { provide: HttpService, useValue: jest.fn() },
+        { provide: ConfigService, useValue: { get: jest.fn() } },
+      ],
     }).compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
